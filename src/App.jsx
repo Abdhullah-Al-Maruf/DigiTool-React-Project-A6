631b8310587e2bc38fc38c045e682169120f07f7
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-03-31 10:21:16 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from './component/Navbar/Navbar'
 import Banner from './component/Banner/Banner'
 import './App.css'
 import RatingSection from './ui/RatingSection'
-
+import Footer from './component/Footer/Footer'
 function App() {
 
 
@@ -15,10 +15,11 @@ function App() {
 
 <main className='space-y-10'>
 <RatingSection/>
-
+<Footer></Footer>
 </main>
     </>
   )
 }
 
 export default App
+
